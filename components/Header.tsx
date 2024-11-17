d97c78c2563c50d84703df6feca604bf758aadf5
proxrustes <proxrustes@gmail.com>
2024-11-17 13:13:32 +0100

    init

---
+++ components/Header.tsx
@@ -0,0 +1,12 @@
+import { Container, Stack, Typography } from "@mui/material";
+
+export function Header() {
+  return (
+    <Container maxWidth="lg" sx={{ mb: 2 }}>
+      <Stack direction="row" justifyContent="space-between">
+        <Typography sx={{ fontWeight: 800 }}>HydroStations</Typography>
+        <Typography>AKA</Typography>
+      </Stack>
+    </Container>
+  );
+}
